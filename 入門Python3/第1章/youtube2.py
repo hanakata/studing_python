befd7e08102d0708581c8a0ef270dd9d2466e170
katayama <katayama@LAPTOP-1MJFCPEF>
2018-10-09 21:25:51 +0900

    1章読了

---
+++ 入門Python3/第1章/youtube2.py
@@ -0,0 +1,7 @@
+import requests
+from urllib.request import urlopen
+url = "https://raw.githubusercontent.com/koki0702/introducing-python/master/dummy_api/youTube_top_rated.json"
+response = requests.get(url)
+data = response.json()
+for video in data['feed']['entry'][0:6]:
+    print(video['title']['$t'])
